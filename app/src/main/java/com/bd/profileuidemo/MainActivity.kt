447e286823cd34a894e9489c1686b190722f5ccf
subroto <subroto_mohonto@yahoo.com>
2024-10-01 00:05:35 +0600

    remove text

--- app/src/main/java/com/bd/profileuidemo/MainActivity.kt
+++ app/src/main/java/com/bd/profileuidemo/MainActivity.kt
@@ -106,10 +106,6 @@ private fun profile(){
                     start.linkTo(parent.start, margin = 24.dp)
                 })
 
-
-            //Hello testing
-
-
             Image(
                 painterResource(R.drawable.write),
                 contentDescription = null,
